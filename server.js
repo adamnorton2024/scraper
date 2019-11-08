2f978f44fc74f08cce635dcdf8a270182086cc68
Adam Norton <adam@adamnorton.com>
2019-11-07 20:08:24 -0600

    server is connecting, pulling titles and links, models are setup.

---
+++ server.js
@@ -0,0 +1,108 @@
+// Dependencies
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure Middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB 
+// mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI);
+
+// Routes
+
+// Scrapes the artnews website for the article data
+// app.get("/scraped", function (req, res) {
+//     axios.get("http://www.artnews.com/category/news/").then(function (response) {
+//         var $ = cheerio.load(response.data);
+
+//         $("h2.entry-title").each(function (i, element) {
+//             var result = {};
+
+//             result.title = $(element).text();
+
+//             result.link = $(element).children("a").attr("href");
+
+//             result.summary = $(element).siblings(".entry-summary").text().trim();
+
+//             db.Article.create(result)
+//                 .then(function (dbArticle) {
+//                     console.log(dbArticle);
+//                 })
+//                 .catch(function (err) {
+//                     console.log(err);
+//                 });
+//         });
+//     });
+//     res.send("Scrape Complete");
+// });
+
+app.get("/scraped", function (req, res) {
+    axios.get("http://www.adventure.com").then(function (response) {
+        var $ = cheerio.load(response.data);
+
+        $("h3").each(function (i, element) {
+            var result = {};
+
+            result.title = $(element).children("a").text();
+            result.link = $(element).children("a").attr("href");
+           // result.image = $(element).children("img").attr("src");
+
+            //result.summary = $(element).siblings(".entry-summary").text().trim();
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+    });
+    res.send("Scrape Complete");
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res){
+    //TODO: finish the route so it grabs all articles
+});
+
+// Route for grabbing specific article by id, populate with it's note. 
+app.get("/articles/:id", function(req, res){
+    //TODO: finish the route so that it uses req.params.id then responds with the article and note included.
+});
+
+// Route of saving/updating an Article's assciated comments
+app.post("/articles/:id", function(req, res){
+    // TODO 
+    //===
+    // save the new comment posted to the Comments Collection, then find an article from the req.params.id and update it's "comment" property with the _id of the new note. 
+});
+
+app.listen(PORT, function(){
+    console.log("App running on port " + PORT + "!");
+});
+
+
+
